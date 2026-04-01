8500f948d73ed2ec394914ea8800e9c113dc8b6e
[OliStew] <[o.stew06@outlook.com]>
2026-04-01 17:51:36 -0500

    Making the starter Global html, js and css files.

---
+++ script.js
@@ -0,0 +1,3 @@
+/* This will be the Global JavaScript File. Anything that will need to be consist between all pages will be put here.
+    Anything else create your own js file and link it in the HTML file.
+    Of course first test it in your own file, but once you are sure it works, put it here. */
