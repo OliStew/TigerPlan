adf56b8ed3c415e974b80ba2df5aade84a8ac3d6
Jrobneal <Jrobneal@hotmail.com>
2026-04-07 15:15:13 -0500

    Create plans.js

---
+++ routes/plans.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getPlans,
+  getPlan,
+  createPlan,
+  updatePlan,
+  deletePlan,
+  addCourseToPlan,
+  updatePlanCourse,
+  removeCourseFromPlan,
+  getSemesterCourses,
+  duplicatePlan
+} = require('../controllers/planController');
+
+// Plan routes (Person 1 will add auth middleware later)
+router.get('/', getPlans);
+router.post('/', createPlan);
+router.get('/:id', getPlan);
+router.put('/:id', updatePlan);
+router.delete('/:id', deletePlan);
+router.post('/:id/duplicate', duplicatePlan);
+
+// Plan course routes
+router.post('/:id/courses', addCourseToPlan);
+router.put('/:id/courses/:courseId', updatePlanCourse);
+router.delete('/:id/courses/:courseId', removeCourseFromPlan);
+router.get('/:id/semesters/:semester/:year', getSemesterCourses);
+
+module.exports = router;
