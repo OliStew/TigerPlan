1e5b2777b163cce9d6c73a2fdc05c9e083f6e529
[OliStew] <[o.stew06@outlook.com]>
2026-04-07 21:54:26 -0500

    Commiting just to commit, to revert to previous commit.

--- script.js
+++ script.js
@@ -7,20 +7,20 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-         document.getElementById("header").style.padding = "10px 10px";
-        document.getElementById("logo").style.fontSize = "15px";
+         document.getElementsByClassName("header")[0].style.padding = "10px 10px";
+        document.getElementsByClassName("header")[0].getElementsByClassName("logo")[0].style.fontSize = "15px";
 
         
-         document.getElementById("navbar").style.opacity = "0%"
-        document.getElementById("navbar").style.top = "-75px";
+         document.getElementsByClassName("navbar")[0].style.opacity = "0%"
+        document.getElementsByClassName("navbar")[0].style.top = "-75px";
 
     
   } else {
-        document.getElementById("header").style.padding = "20px 10px";
-        document.getElementById("logo").style.fontSize = "25px";
+        document.getElementsByClassName("header")[0].style.padding = "20px 10px";
+        document.getElementsByClassName("header")[0].getElementsByClassName("logo")[0].style.fontSize = "25px";
 
-        document.getElementById("navbar").style.opacity = "100%"
-        document.getElementById("navbar").style.top = "0px";
+        document.getElementsByClassName("navbar")[0].style.opacity = "100%"
+        document.getElementsByClassName("navbar")[0].style.top = "0px";
 
 
   }
