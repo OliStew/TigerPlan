3bafe0b08f87bd25dc8ac0937d52d375d5d114f0
[OliStew] <[o.stew06@outlook.com]>
2026-04-14 11:09:43 -0500

    skill-tags map created and workinng

--- runCareerDataBase.js
+++ runCareerDataBase.js
@@ -4,20 +4,29 @@ const postContainer = document.querySelector(".careerContent");
 
 export const postMethods = (dataArray) => {
 
-    postContainer.innerHTML = ""; 
+    postContainer.innerHTML = "";
 
     dataArray.map((postData) => {
         const postElement = document.createElement('div');
         postElement.classList.add("card");
+
+        const skillsHTML = postData.jobSkill.split(' ').map(skill => {
+            return `<p class="jobSkillTag">${skill}</p>`;
+        }).join('');
+
         postElement.innerHTML = ` 
             <p class="job-name"> <i class="material-icons suitCase"
             style="font-size:23px; color: #461D7C;">business_center</i> ${postData.jobName}</p>
             <p class="job-details">${postData.jobDetails}</p>
             <p class="job-pay"> <i class="material-icons dollarSign"
             style="font-size:15px; color: #808080;">attach_money</i>${postData.jobPay}</p>
+           
             <p id="p1">Required Skills:</p>
-            <p class="job-skills">Skills SKills</p>    
-        `
+
+            <div class="job-skills"> 
+            ${skillsHTML}
+            </div>    
+        `;
         postContainer.appendChild(postElement)
     })
 }
