29b728959fd91dbb2f95e9d8f9500891c0ef62b6
Amiah Humphrey <dearah54@gmail.com>
2026-04-15 10:02:34 -0500

    updated com sci, bio, and business Ad careers

--- careerSearchDataBase.js
+++ careerSearchDataBase.js
@@ -2,46 +2,46 @@ export const cardDataComputerScience = [
     {
         jobName:'Entry-Level Full Stack Developer',
         jobDetails: 'Assists in designing, developing, testing, and maintaining software applications.',
-        jobPay: '$30-40/hr',
-        jobSkill: 'Basic knowledge of programming languages (Java, Python, C++, etc.)'
+        jobPay: '$62-83K/yr',
+        jobSkill: 'Java, JavaScript, Python, C++, HTML '
     },
 
      {
         jobName:'Software Developer',
         jobDetails: 'This position is a high impact, executive sponsored talent acceleration program designed to rapidly onboard, enable, and activate early career professionals.',
         jobPay: '$70-80K/yr',
-        jobSkill: 'Agentic AI solutions, automation concepts, or AI enabled workflows.'
+        jobSkill: 'DesignPatterns, Databases, Collaboration, OOP, Programming '
     },
 
      {
         jobName:'Digital Asset Manager',
         jobDetails: 'Owning and maintaining the Digital Asset Management (DAM) system ensuring its optimal functionality.',
-        jobPay: '$30-40/hr',
-        jobSkill: 'Ability to review usage reports and analytics to improve asset performance and system efficiency.'
-    }
+        jobPay: '$63-85K/yr',
+        jobSkill: ' Metadata, Databases, Anylytics, Collaboration, Detail-Oriented '
+     }
 
 ];
 
 export const cardDataBiology = [
      {
         jobName:'Medicine Physicaian',
-        jobDetails: 'provide direct care for patients while following established protocols and procedures for patient care standards.',
+        jobDetails: 'Provide direct care for patients while following established protocols and procedures for patient care standards.',
         jobPay: '$208-323K/yr',
-        jobSkill: 'Demonstrates strong interpersonal skills, excellent clinical judgement, communication skills, and quality assessment skills.'
+        jobSkill: 'Interpersonal-skills, Clinical-judgement, Communication , Quality-assessment, Patient-care '
     },
 
      {
         jobName:'Nurse',
-        jobDetails: 'fix the human',
-        jobPay: '500,000-600,000',
-        jobSkill: 'eye-chart temperature'
+        jobDetails: 'Provides patient care by assessing conditions, administering medicatins, monitoring vial signs, assiting with treatments, and collaborating with healthcare teams to ensure safe and effective care.',
+        jobPay: '$500-600K/yr',
+        jobSkill: 'Multitasking, Adaptability, Medication-administraion, Critical-thinking'
         
     },
     {
         jobName:'Forensic Scientist',
         jobDetails: 'Apply scientific principles, techniques and laboratory methods to analyze phyical evidence from crime scenes.',
         jobPay: '$58-67K/yr',
-        jobSkill: 'Expertise in laboratory equipment, data analysis software, and scientific methodologies.'
+        jobSkill: 'Evidence-collection, Trace-analysis, Scientific-methodologies, Critical-thinking'
         
     }
     
@@ -50,45 +50,74 @@ export const cardDataBiology = [
 
 export const cardDataBusinessAdministration = [
      {
-        jobName:'fisherman',
-        jobDetails: 'Gets the fish',
-        jobPay: '200,000-300,000'
+        jobName:'Business Analyst',
+        jobDetails: 'Analyzes data and processes to help businesses improve performance and make more effective decisions.',
+        jobPay: '$70-95K/yr',
+        jobSkill:'Data-analysis, Excel, SQL, Problem-solving, Communication'
     },
 
+
      {
-        jobName:'doctor',
-        jobDetails: 'fix the human',
-        jobPay: '500,000-600,000'
+        jobName:'Human Resources Manager',
+        jobDetails: ' Manages hiring, employee relations, and workplace policies to support company staff.',
+        jobPay: '$75-110K/yr',
+        jobSkill:'Recruiting, Conflict-resolution, Organization, Onboarding, Communication'
+    },
+
+    {
+        jobName:'Marketing Manager',
+        jobDetails: 'Develops straegies to promate products and increase brand awareness and sales.',
+        jobPay: '$80-120K/yr',
+        jobSkill:'Branding, Analytics, Advertising, SEO, Creativity'
     }
 ];
 
 
 export const cardDataEngineering = [
      {
-        jobName:'fisherman',
-        jobDetails: 'Gets the fish',
-        jobPay: '200,000-300,000'
+        jobName:'doctor',
+        jobDetails: 'fix the human',
+        jobPay: '500,000-600,000',
+        jobSkill:''
     },
+
 
      {
         jobName:'doctor',
         jobDetails: 'fix the human',
-        jobPay: '500,000-600,000'
+        jobPay: '500,000-600,000',
+        jobSkill:''
+    },
+
+    {
+        jobName:'doctor',
+        jobDetails: 'fix the human',
+        jobPay: '500,000-600,000',
+        jobSkill:''
     }
 ];
 
 
 export const cardDataEconomics = [
-     {
-        jobName:'fisherman',
-        jobDetails: 'Gets the fish',
-        jobPay: '200,000-300,000'
-    },
-
-     {
+    {
         jobName:'doctor',
         jobDetails: 'fix the human',
-        jobPay: '500,000-600,000'
+        jobPay: '500,000-600,000',
+        jobSkill:''
+    },
+
+    {
+        jobName:'doctor',
+        jobDetails: 'fix the human',
+        jobPay: '500,000-600,000',
+        jobSkill:''
+    },
+    {
+        jobName:'doctor',
+        jobDetails: 'fix the human',
+        jobPay: '500,000-600,000',
+        jobSkill:''
     }
+
 ];
 
